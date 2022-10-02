9d9a12f02bca57dc9a2606387b19906b7df07867
BogAlex4ndr <bog.alex4ndr@gmail.com>
2022-10-02 10:47:43 +0300

    tetris v1

---
+++ bg.js
@@ -0,0 +1,25 @@
+let changeBg = document.querySelector('.change__bg');
+let clickCounter = 0;
+changeBg.addEventListener('click', function (e) {
+    if(clickCounter < 1) {
+        clickCounter++
+        document.querySelector('.tetris').classList.add('tetris__bg');
+    } else if (clickCounter < 2) {
+        clickCounter++
+        document.querySelector('.tetris').classList.replace('tetris__bg','tetris__bg2'); 
+    } else if (clickCounter < 3) {
+        clickCounter++
+        document.querySelector('.tetris').classList.replace("tetris__bg2",'tetris__bg3');    
+    } else if (clickCounter < 4) {
+        clickCounter++
+        document.querySelector('.tetris').classList.replace("tetris__bg3",'tetris__bg4');    
+    }else if (clickCounter < 5) {
+        clickCounter++
+        document.querySelector('.tetris').classList.replace("tetris__bg4",'tetris__bg5');    
+    }
+    else {
+        clickCounter = 0;
+        document.querySelector('.tetris').classList.remove('tetris__bg5');
+    }
+    console.log(clickCounter)
+})
